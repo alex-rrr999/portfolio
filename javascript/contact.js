a7441093160ec42385bcb5b575559a663be7f2ec
alex-rrr999 <rodrialex.edu@gmail.com>
2023-02-18 22:26:20 -0800

    Alpha commit; work in progress.

---
+++ javascript/contact.js
@@ -0,0 +1,123 @@
+function dropdown() {
+  var x = document.getElementById("myLinks");
+  x.classList.toggle("show");
+}
+
+
+// working FADE OUT
+document.addEventListener('DOMContentLoaded', function(event) {
+  document.body.classList.add('load');
+
+
+  const links = document.querySelectorAll('a');
+
+  links.forEach(link => {
+    const href = link.getAttribute('href'); // move
+    link.addEventListener('click', function(event) {
+      event.preventDefault();
+  
+      // Add the "fade-out" class to the body
+      console.log(href)
+      if (href === "/index.html" || href === "/pages/about.html" || href === "/pages/projects.html" || href === "/pages/contact.html") {
+      console.log("yes")
+      document.body.classList.add('fade-out');
+      }
+      
+      // Wait 
+
+      if (href === "https://www.instagram.com/alex.rrr999/" || href === "https://github.com/alex-rrr999" || href === "https://www.linkedin.com/in/alex-rrr999/" || href === "https://www.youtube.com/@alex-rrr999")
+      setTimeout(function() {
+        // window.location.href = href;
+        window.open(href);
+      }, 100);
+      else {
+      setTimeout(function() {
+        window.location.href = href;
+      }, 200);
+      }
+    });
+  });
+});
+
+
+
+
+document.addEventListener('DOMContentLoaded',function(event){
+  var dataText = ["Reach Me"];
+  
+
+  function typeWriter(text, i, fnCallback) {
+    // chekc if text isn't finished yet
+    if (i < (text.length)) {
+      // add next character to h1
+     document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+
+      // wait for a while and call this function again for next character
+      setTimeout(function() {
+        typeWriter(text, i + 1, fnCallback)
+      }, 100);
+    }
+    // text finished, call callback if there is a callback function
+    else if (typeof fnCallback == 'function') {
+      // call callback after timeout
+      setTimeout(fnCallback, 700);
+    }
+  }
+  // start a typewriter animation for a text in the dataText array
+   function StartTextAnimation(i) {
+     if (typeof dataText[i] == 'undefined'){
+        setTimeout(function() {
+          StartTextAnimation(0);
+        }, 20000);
+     }
+     // check if dataText[i] exists
+    if (i < dataText[i].length) {
+      // text exists! start typewriter animation
+     typeWriter(dataText[i], 0, function(){
+       // after callback (and whole text has been animated), start next text
+
+     });
+    }
+  }
+  // start the text animation
+  StartTextAnimation(0);
+
+
+
+  const email = document.querySelector('.email');
+  email.addEventListener('click', () => {
+    const emailText = email.innerText;
+    navigator.clipboard.writeText(emailText)
+      .then(() => {
+        // Check if a tooltip element already exists
+        let tooltip = document.querySelector('.tooltip');
+        if (!tooltip) {
+          // If a tooltip doesn't exist, create a new one
+          tooltip = document.createElement('div');
+          tooltip.classList.add('tooltip');
+          tooltip.innerText = 'Email copied to clipboard';
+          email.parentNode.insertBefore(tooltip, email.nextSibling);
+        }
+        
+        // Fade in the tooltip
+        setTimeout(() => {  tooltip.style.opacity = 1; }, 25);
+    
+        // Remove the tooltip after 2 seconds
+        setTimeout(() => {
+          tooltip.style.opacity = 0;
+        }, 2000);
+      })
+      .catch((error) => {
+        console.error(`Error copying email to clipboard: ${error}`);
+      });
+  });
+  
+
+
+});
+
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,24 @@
+# Portfolio
+
+This project is a portfolio website showcasing my works. It is built using HTML, CSS, and JavaScript. The website is currently under development and not yet finished.
+
+## Technologies Used
+
+The following technologies were used in the development of this project:
+- HTML
+- CSS
+- JavaScript
+
+## Deployment
+
+This project is deployed using Netlify. You can access the deployed version of the website at https://projektalex.com
+
+## Special Thanks
+
+I would like to extend a special thanks to [@bautista73](https://github.com/bautista73) for their assistance and support during the development of this project.
+
+## Project Description
+
+This project is a personal portfolio website that showcases my works. The website is designed to provide potential clients and employers with an overview of my skills and experience. The website is currently a work in progress, and new features and updates will be added in the future.
+
+Thanks.
